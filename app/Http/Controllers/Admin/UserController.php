4bd679c3c2007f3b7f20241becf0fac452dd2065
Ricardo <rick_ferry1@hotmail.com>
2024-06-26 13:02:12 -0300

    Dependencies e modal

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,4 +14,32 @@ class UserController extends Controller
 
         return $users;
     }
+
+    public function store()
+    {
+        request()->validate([
+            'email' => 'required|unique:users,email',
+        ]);
+
+        return User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password')),
+        ]);
+    }
+
+    public function update(User $user)
+    {
+        request()->validate([
+            'email' => 'required|unique:users,email,' . $user->id,
+        ]);
+
+        $user->update([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => request('password') ? bcrypt(request('password')) : $user->password,
+        ]);
+
+        return $user;
+    }
 }
--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -1,14 +1,84 @@
 <script setup>
 import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, reactive } from 'vue';
+import { Form, Field, useResetForm } from 'vee-validate';
+import * as yup from 'yup';
 
 const users = ref([]);
+const editing = ref(false);
+const formValues = ref();
+const form = ref(null);
 
 const getUsers = () => {
     axios.get('/api/users')
-    .then((response) => {
-        users.value = response.data;
-    })
+        .then((response) => {
+            users.value = response.data;
+        })
+}
+
+const createUserSchema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required().min(8),
+});
+
+const editUserSchema = yup.object({
+    name: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().when((password, schema) => {
+        return password ? schema.required().min(8) : schema;
+    }),
+});
+
+const createUser = (values, { resetForm, setErrors }) => {
+    axios.post('/api/users', values)
+        .then((response) => {
+            users.value.unshift(response.data);
+            $('#userFormModal').modal('hide');
+            resetForm();
+        })
+        .catch((error) => {
+            if (error.response.data.errors) {
+                setErrors(error.response.data.errors);
+            }
+        })
+};
+
+const addUser = () => {
+    editing.value = false;
+    $('#userFormModal').modal('show');
+};
+
+const editUser = (user) => {
+    editing.value = true;
+    form.value.resetForm();
+    $('#userFormModal').modal('show');
+    formValues.value = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+    };
+};
+
+const updateUser = (values, {setErrors}) => {
+    axios.put('/api/users/' + formValues.value.id, values)
+        .then((response) => {
+            const index = users.value.findIndex(user => user.id === response.data.id);
+            users.value[index] = response.data;
+            $('#userFormModal').modal('hide');
+        }).catch((error) => {
+            setErrors(error.response.data.errors);
+            console.log(error);
+        });
+}
+
+const handleSubmit = (values, actions) => {
+    // console.log(actions);
+    if (editing.value) {
+        updateUser(values, actions);
+    } else {
+        createUser(values, actions);
+    }
 }
 
 onMounted(() => {
@@ -36,6 +106,9 @@ onMounted(() => {
 
     <div class="content">
         <div class="container-fluid">
+            <button @click="addUser" type="button" class="mb-2 btn btn-primary">
+                Add New User
+            </button>
             <div class="card">
                 <div class="card-body">
                     <table class="table table-bordered">
@@ -56,11 +129,58 @@ onMounted(() => {
                                 <td>{{ user.email }}</td>
                                 <td>-</td>
                                 <td>-</td>
-                                <td>-</td>
+                                <td>
+                                    <a href="#" @click.prevent="editUser(user)"><i class="fa fa-edit"></i></a>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="userFormModal" data-backdrop="static" tabindex="-1" role="dialog"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">
+                        <span v-if="editing">Edit User</span>
+                        <span v-else>Add New User</span>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <Form ref="form" @submit="handleSubmit" :validation-schema="editing ? editUserSchema : createUserSchema" v-slot="{ errors }" :initial-values="formValues">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <Field name="name" type="text" class="form-control" :class="{'is-invalid': errors.name }" id="name"
+                                aria-describedby="nameHelp" placeholder="Enter full name" />
+                            <span class="invalid-feedback">{{ errors.name }}</span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <Field name="email" type="email" class="form-control " :class="{'is-invalid': errors.email }" id="email"
+                                aria-describedby="nameHelp" placeholder="Enter full name" />
+                            <span class="invalid-feedback">{{ errors.email }}</span>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="email">Password</label>
+                            <Field name="password" type="password" class="form-control " :class="{'is-invalid': errors.password }" id="password"
+                                aria-describedby="nameHelp" placeholder="Enter password" />
+                            <span class="invalid-feedback">{{ errors.password }}</span>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </Form>
             </div>
         </div>
     </div>
