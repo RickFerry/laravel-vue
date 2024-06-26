a338a7401f66e9fa4a311b21ab64ae2589bb285a
Ricardo <rick_ferry1@hotmail.com>
2024-06-26 15:26:29 -0300

    Table, picture, controller, blade, ...

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | Starter</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="icon" type="image/png" sizes="16x16" href="https://public-files.gumroad.com/7cgkhx8y9f72akfe4iikrqhwkdxs">
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -158,10 +159,10 @@
 
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="https://adminlte.io/themes/v3/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ auth()->user()->avatar }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">{{ auth()->user()->name }}</a>
                     </div>
                 </div>
 
